2ceedf93bae64f696df8d5e4fadf373ddf3eeda5
Edgar h Florez diaz <edgarhflorezdiaz@Edgars-MacBook-Pro-2.local>
2024-11-13 21:44:29 -0500

    Game complete, with the state in the game passing props to board and square

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ function Square({ value, onSquareClicked }) {
   );
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
-  console.log("body");
-
+function Board({ xIsNext, squares, onPlay }) {
   let winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -28,10 +23,8 @@ function Board() {
     }
 
     const nextSquares = squares.slice();
-    const nextSymbol = xIsNext ? "X" : "0";
-    nextSquares[i] = nextSymbol;
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    nextSquares[i] = xIsNext ? "X" : "0";
+    onPlay(nextSquares);
   }
 
   return (
@@ -57,15 +50,43 @@ function Board() {
 }
 
 export default function Game() {
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+
+  function handlePlay(nextSquares) {
+    const nexStory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nexStory);
+    setCurrentMove(nexStory.length - 1);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to move #${move}`;
+    } else {
+      description = "Go to Game Start";
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>Game history </ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
@@ -83,8 +104,6 @@ const winningLines = [
 ];
 
 function calculateWinner(squares) {
-  console.log("calculate winner exe");
-
   for (const [a, b, c] of winningLines) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
